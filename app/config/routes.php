ae306c57814eac6489771821e25bf2f46fb4d546
Thomas J. Fazanaro <tfazanaro@gmail.com>
2015-07-10 14:18:37 -0300

    Alterado o namespace (diretório) da classe Route.

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,15 +1,13 @@
 <?php
 
 /*
- * Você deve definir o nome que será usado
- * na url para acessar o controlador.
+ * Você deve definir o nome da rota que será
+ * usado na url para acessar o controlador.
  */
 $route->controller('home', 'Home');
-$route->controller('welcome', 'Home');
 $route->controller('login', 'Login');
 $route->controller('register', 'Register');
 $route->controller('user', 'UserController');
-$route->controller('debug', 'Debug');
 
 /*
  * Você pode definir URLs mais amigáveis para acessar
