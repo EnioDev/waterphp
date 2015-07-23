a2498640242123048db32d0a6ad92fc70a9a9b72
Thomas J. Fazanaro <tfazanaro@gmail.com>
2015-07-23 09:08:34 -0300

    Tratado os valores dos campos do formulário para limpar após salvar.

--- app/view/user/register.php
+++ app/view/user/register.php
@@ -21,11 +21,24 @@
                     </div>
                 <?php endif; ?>
 
+                <?php
+                    $name = '';
+                    $email = '';
+                    $password = '';
+
+                    if (isset($errors) and count($errors) > 0)
+                    {
+                        $name = $app::old('name');
+                        $email = $app::old('email');
+                        $password = $app::old('password');
+                    }
+                ?>
+
                 <input type="hidden" name="_token" value="<?php echo $app::token(); ?>">
 
-                <input name="name" type="text" class="form-control no-radius-bottom" value="<?php echo $app::old('name'); ?>" placeholder="<?php echo $app::strings()->user->fields->name; ?>" autofocus>
-                <input name="email" type="email" class="form-control no-radius" value="<?php echo $app::old('email'); ?>" placeholder="<?php echo $app::strings()->user->fields->email; ?>">
-                <input name="password" type="password" class="form-control no-radius-top" value="<?php echo $app::old('password'); ?>" placeholder="<?php echo $app::strings()->user->fields->password; ?>">
+                <input name="name" type="text" class="form-control no-radius-bottom" value="<?php echo $name; ?>" placeholder="<?php echo $app::strings()->user->fields->name; ?>" autofocus>
+                <input name="email" type="email" class="form-control no-radius" value="<?php echo $email; ?>" placeholder="<?php echo $app::strings()->user->fields->email; ?>">
+                <input name="password" type="password" class="form-control no-radius-top" value="<?php echo $password; ?>" placeholder="<?php echo $app::strings()->user->fields->password; ?>">
 
                 <input type="submit" class="btn btn-lg btn-success btn-block" name="submit" value="<?php echo $app::strings()->user->buttons->register; ?>">
                 <a class="btn btn-lg btn-primary btn-block" href="<?php echo $app::route('login'); ?>"><?php echo $app::strings()->user->buttons->login; ?></a>
