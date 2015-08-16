1ea1f6bf132072c845ac43251813d0d5cfa921e9
Thomas J. Fazanaro <tfazanaro@gmail.com>
2015-08-16 16:45:52 -0300

    Alterado as informações de conexão com o banco.

--- app/config/config.php
+++ app/config/config.php
@@ -103,19 +103,21 @@ define('ENCRYPTION_KEY', 'your_secret_key');
  * BANCO DE DADOS
  * --------------------------------------------------------------------------
  *
- * Defina as informações abaixo corretamente para usar o seu banco de dados.
+ * Atenção: Defina as informações abaixo corretamente, caso contrário, uma
+ * exceção será lançada sempre que tentar executar uma operação de banco
+ * de dados.
  *
- * Atenção: Consulte os drivers disponíveis para fazer a conexão com o banco
- * usando o PDO (PHP Data Objects). Exemplo: Para se conectar ao postgresql
- * você deve usar o drive "pgsql".
+ * Exemplo: Para fazer a conexão com o Postgresql defina o driver "pgsql".
+ * Consulte os drivers disponíveis para fazer a conexão com os demais
+ * bancos usando o PDO (PHP Data Objects).
  */
 
 define('DB_DRIVER'  , 'mysql');
-define('DB_HOST'    , 'localhost');
-define('DB_PORT'    , '5432');
-define('DB_NAME'    , 'waterphp');
-define('DB_USER'    , 'root');
-define('DB_PASSWORD', 'root');
+define('DB_HOST'    , 'localhost'); // Informe o nome do servidor ou domínio.
+define('DB_PORT'    , '');          // Informe a porta configurada para conectar-se ao banco. Ex: 3306 (padrão Mysql) ou 5432 (padrão Postgresql).
+define('DB_NAME'    , '');          // Informe o nome do banco de dados que você criou para usar com a aplicação.
+define('DB_USER'    , '');          // Informe o usuário para fazer a conexão com o banco.
+define('DB_PASSWORD', '');          // Informe a senha do usuário caso definida.
 
 /*
  * --------------------------------------------------------------------------
@@ -133,4 +135,4 @@ define('MAIL_IS_HTML'   , true);
 define('MAIL_CHARSET'   , 'iso-8859-1');
 define('MAIL_FROM'      , ''); // user@domain.com
 define('MAIL_SMTP_HOST' , ''); // ssl://smtp.gmail.com
-define('MAIL_SMTP_PORT' , 25); // 465
+define('MAIL_SMTP_PORT' , ''); // 465
