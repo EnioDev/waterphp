27494f6f9d45b11ce8a4f93b9797e994b3f7c0f3
Thomas J. Fazanaro <tfazanaro@gmail.com>
2015-12-14 18:25:40 -0200

    Removido a constante SESSION_TIMEOUT e renomeado a constante APP_LANGUAGE para DEFAULT_LANGUAGE.

--- app/config/config.php
+++ app/config/config.php
@@ -14,10 +14,12 @@
 define('DEBUG_MODE', 0);
 
 /*
- * Você pode definir seu próprio modelo de página para depurar os erros,
+ * Você pode criar seu próprio modelo de página para depurar os erros,
  * tais como "Warning" e "Notice", caso a opção acima esteja ativada.
- * Veja o modelo dado com o framework para saber quais informações você
- * pode exibir.
+ * Veja o template usado na aplicação de exemplo para saber quais
+ * informações você pode exibir na visão.
+ *
+ * Atenção: Nunca apague um template antes de redefiní-lo.
  */
 
 define('DEBUG_VIEW', 'template/debug');
@@ -26,6 +28,8 @@ define('DEBUG_VIEW', 'template/debug');
  * Você pode definir seu próprio modelo de página de erro 404.
  * Esta página será exibida sempre que o controlador ou método informado
  * na URL não existir.
+ *
+ * Atenção: Nunca apague um template antes de redefiní-lo.
  */
 
 define('ERROR_404_VIEW', 'template/404');
@@ -42,18 +46,12 @@ define('ERROR_404_VIEW', 'template/404');
 define('SESSION_LIFETIME', '7200'); // 7200 = 2h
 
 /*
- * Se preferir, você pode definir o tempo máximo em segundos que a sessão
- * do usuário deve durar mesmo que ele esteja ativo.
- */
-define('SESSION_TIMEOUT', '0');
-
-/*
  * ---------------------------------------------------------------------------
  * CONTROLADOR
  * --------------------------------------------------------------------------
  *
  * Você deve definir um controlador padrão para ser executado caso nenhum
- * controlador tenha sido informado na url.
+ * controlador tenha sido informado na url do projeto.
  */
 
 define('CONTROLLER_INDEX', 'Home');
@@ -63,11 +61,12 @@ define('CONTROLLER_INDEX', 'Home');
  * IDIOMA
  * --------------------------------------------------------------------------
  *
- * Você deve definir o idioma da aplicação. Esta ação é útil para ajudar o
- * buscador a classificar seu site no idioma apropriado, orientar os
- * navegadores a exibir acentuação e caracteres especiais corretamente.
+ * Você deve definir o idioma padrão da aplicação. Esta ação é útil para
+ * ajudar o buscador a classificar seu site no idioma apropriado, orientar
+ * os navegadores a exibir acentuação e caracteres especiais corretamente.
+ *
  * O idioma também é usado para fazer a tradução da aplicação. Consulte
- * a documentação para saber mais.
+ * a classe Lang na documentação para saber mais.
  *
  * Veja alguns valores possíveis:
  *
@@ -83,9 +82,10 @@ define('CONTROLLER_INDEX', 'Home');
  * ru Russo
  * zh Chinês
  * ja Japonês
+ *
  */
 
-define('APP_LANGUAGE', 'pt-br');
+define('DEFAULT_LANGUAGE', 'pt-br');
 
 /*
  * --------------------------------------------------------------------------
@@ -109,36 +109,36 @@ define('ENCRYPTION_KEY', 'your_secret_key');
  * BANCO DE DADOS
  * --------------------------------------------------------------------------
  *
- * Atenção: Defina as informações abaixo corretamente, caso contrário, uma
- * exceção será lançada sempre que tentar executar uma operação de banco
- * de dados.
+ * Você deve definir as informações abaixo para fazer a conexão com o banco
+ * de dados da sua aplicação.
  *
- * Exemplo: Para fazer a conexão com o Postgresql defina o driver "pgsql".
- * Consulte os drivers disponíveis para fazer a conexão com os demais
- * bancos usando o PDO (PHP Data Objects).
+ * Atenção: Defina as informações corretamente, caso contrário, uma exceção
+ * será lançada sempre que tentar executar uma operação de banco de dados
+ * ou se estiver usando a aplicação de exemplo dada com o framework.
+ *
+ * Consulte os drivers disponíveis na documentação do PHP para fazer a
+ * conexão com os diversos bancos de dados usando o PDO (PHP Data Objects).
+ * Exemplo: use 'mysql' para Mysql ou 'pgsql' para Postgresql.
  */
 
-define('DB_DRIVER'  , 'mysql');
-define('DB_HOST'    , 'localhost'); // Informe o nome do servidor ou domínio.
-define('DB_PORT'    , '');          // Informe a porta configurada para conectar-se ao banco. Ex: 3306 (padrão Mysql) ou 5432 (padrão Postgresql).
-define('DB_NAME'    , '');          // Informe o nome do banco de dados que você criou para usar com a aplicação.
-define('DB_USER'    , '');          // Informe o usuário para fazer a conexão com o banco.
-define('DB_PASSWORD', '');          // Informe a senha do usuário caso definida.
+define('DB_DRIVER'  , ''); // Informe o driver desejado para conectar-se ao banco.
+define('DB_HOST'    , ''); // Informe o nome do servidor ou domínio. Ex: localhost.
+define('DB_PORT'    , ''); // Informe a porta para conectar-se ao banco. Ex: 3306 (Mysql) ou 5432 (Postgresql).
+define('DB_NAME'    , ''); // Informe o nome do banco que você criou para usar com a aplicação.
+define('DB_USER'    , ''); // Informe o usuário para fazer a conexão com o banco.
+define('DB_PASSWORD', ''); // Informe a senha do usuário definido acima.
 
 /*
  * --------------------------------------------------------------------------
  * E-MAIL
  * --------------------------------------------------------------------------
  *
- * Atenção: Para usar a classe Mail certifique-se que o PHP instalado em seu
- * servidor está corretamente configurado para enviar e-mails usando a função
- * mail do PHP.
- * Você também pode testar as funcionalidades da classe Mail através do seu
- * servidor de hospedagem, provavelmente ele estará corretamente configurado.
+ * Atenção: Certifique-se que o PHP instalado em seu servidor está
+ * corretamente configurado para enviar e-mails usando a função mail do PHP.
  */
 
 define('MAIL_IS_HTML'   , true);
 define('MAIL_CHARSET'   , 'iso-8859-1');
-define('MAIL_FROM'      , ''); // user@domain.com
+define('MAIL_FROM'      , ''); // user@gmail.com
 define('MAIL_SMTP_HOST' , ''); // ssl://smtp.gmail.com
 define('MAIL_SMTP_PORT' , ''); // 465
