0063ef939abe8c8ae97c549f250f39b30794bbd9
Thomas J. Fazanaro <tfazanaro@gmail.com>
2015-12-17 11:35:57 -0200

    Removido os comentários da classe.

--- water/core/base/Model.php
+++ water/core/base/Model.php
@@ -4,11 +4,6 @@ use core\database\Crud;
 
 class Model extends Crud
 {
-    // Você DEVE sobreescrever este atributo na classe filha
-    // e definir o nome da tabela que será usado no modelo.
     protected $table = null;
-
-    // Você DEVE redefinir este atributo na classe filha SE
-    // o nome do campo chave primária na tabela for diferente.
     protected $primary_key = 'id';
 }
