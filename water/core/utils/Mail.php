13d378d9bf36dc3801029140c66c19c423b4e39d
Thomas J. Fazanaro <tfazanaro@gmail.com>
2015-12-17 10:44:24 -0200

    Melhoria no tratamento do método send.

--- water/core/utils/Mail.php
+++ water/core/utils/Mail.php
@@ -29,7 +29,7 @@ final class Mail
         } else if (is_string($to)) {
             $this->to = trim($to);
         } else {
-            $this->to = false;
+            $this->to = null;
         }
     }
 
@@ -38,7 +38,7 @@ final class Mail
         if (is_string($subject) and strlen($subject) > 0) {
             $this->subject = substr(trim($subject), 0, 70);
         } else {
-            $this->subject = '';
+            $this->subject = null;
         }
     }
 
@@ -47,7 +47,7 @@ final class Mail
         if (is_string($message) and strlen($message) > 0) {
             $this->message = wordwrap(trim($message), 70);
         } else {
-            $this->message = false;
+            $this->message = null;
         }
     }
 
@@ -65,11 +65,16 @@ final class Mail
     {
         $this->setTo($to);
 
-        $accept = false;
-
-        if ($this->to and $this->message) {
-            $accept = mail($this->to, $this->subject, $this->message, implode("\r\n", $this->headers), '-f' . MAIL_FROM);
+        if (is_null($this->to)) {
+            throw new \Exception('You can\'t send a message without a recipient.');
         }
-        return $accept;
+        if (is_null($this->subject)) {
+            throw new \Exception('You can\'t send a message without a subject.');
+        }
+        if (is_null($this->message)) {
+            throw new \Exception('You need to define a message to send.');
+        }
+
+        return mail($this->to, $this->subject, $this->message, implode("\r\n", $this->headers), '-f' . MAIL_FROM);
     }
 }
