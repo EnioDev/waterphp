a53a2f579a68fea7128b05aac3222e7c1b5c9b51
Thomas J. Fazanaro <tfazanaro@gmail.com>
2016-01-18 11:04:34 -0200

    Tratado o método make para receber array ou objeto.

--- water/core/utils/Auth.php
+++ water/core/utils/Auth.php
@@ -7,7 +7,11 @@ final class Auth
     public static function make($user)
     {
         self::validateNumArgs(__FUNCTION__, func_num_args(), 1, 1);
-        self::validateArgType(__FUNCTION__, $user, 1, ['object']);
+        self::validateArgType(__FUNCTION__, $user, 1, ['object', 'array']);
+
+        if (is_array($user) and count($user) === 1) {
+            $user = (object) $user[0];
+        }
 
         if (!Session::get('app_session_user') and is_object($user)) {
             Session::set('app_session_user', Encryption::encode(serialize($user)));
