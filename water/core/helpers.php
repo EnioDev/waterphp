a49a783443c05e7ff46fb237ab6cf436e0942f96
Thomas J. Fazanaro <tfazanaro@gmail.com>
2016-01-24 11:37:36 -0200

    Tratado todos os métodos para verificar o número e tipo de argumentos.

--- water/core/helpers.php
+++ water/core/helpers.php
@@ -13,7 +13,9 @@ if (!function_exists('current'))
      */
     function current()
     {
-        return Url::current();
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args());
+
+        return core\utils\Url::current();
     }
 }
 
@@ -21,7 +23,9 @@ if (!function_exists('current_url'))
 {
     function current_url()
     {
-        return Url::current();
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args());
+
+        return core\utils\Url::current();
     }
 }
 
@@ -32,6 +36,8 @@ if (!function_exists('base'))
      */
     function base()
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args());
+
         return core\utils\Url::base();
     }
 }
@@ -40,6 +46,8 @@ if (!function_exists('base_url'))
 {
     function base_url()
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args());
+
         return core\utils\Url::base();
     }
 }
@@ -48,6 +56,10 @@ if (!function_exists('url'))
 {
     function url($str, $params = null)
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args(), 1, 2);
+        core\traits\ClassMethods::validateArgType(__FUNCTION__, $str, 1, ['string']);
+        core\traits\ClassMethods::validateArgType(__FUNCTION__, $params, 2, ['array', 'null']);
+
         return core\utils\Url::base($str, $params);
     }
 }
@@ -56,6 +68,10 @@ if (!function_exists('controller'))
 {
     function controller($controllerName, $params = null)
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args(), 1, 2);
+        core\traits\ClassMethods::validateArgType(__FUNCTION__, $controllerName, 1, ['string']);
+        core\traits\ClassMethods::validateArgType(__FUNCTION__, $params, 2, ['array', 'null']);
+
         return core\utils\Url::controller($controllerName, $params);
     }
 }
@@ -64,6 +80,10 @@ if (!function_exists('route'))
 {
     function route($routeName, $params = null)
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args(), 1, 2);
+        core\traits\ClassMethods::validateArgType(__FUNCTION__, $routeName, 1, ['string']);
+        core\traits\ClassMethods::validateArgType(__FUNCTION__, $params, 2, ['array', 'null']);
+
         return core\utils\Url::route($routeName, $params);
     }
 }
@@ -72,6 +92,9 @@ if (!function_exists('asset'))
 {
     function asset($resource)
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args(), 1, 1);
+        core\traits\ClassMethods::validateArgType(__FUNCTION__, $resource, 1, ['string']);
+
         return core\utils\Url::asset($resource);
     }
 }
@@ -86,6 +109,8 @@ if (!function_exists('public_path'))
 {
     function public_path()
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args());
+
         return PUBLIC_PATH;
     }
 }
@@ -94,6 +119,8 @@ if (!function_exists('image_path'))
 {
     function image_path()
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args());
+
         return IMAGE_PATH;
     }
 }
@@ -108,6 +135,8 @@ if (!function_exists('default_language'))
 {
     function default_language()
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args());
+
         return DEFAULT_LANGUAGE;
     }
 }
@@ -116,6 +145,8 @@ if (!function_exists('session_language'))
 {
     function session_language()
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args());
+
         return core\utils\Session::get('app_session_language');
     }
 }
@@ -133,6 +164,9 @@ if (!function_exists('view'))
      */
     function view($view)
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args(), 1, 1);
+        core\traits\ClassMethods::validateArgType(__FUNCTION__, $view, 1, ['string']);
+
         core\base\View::load($view);
     }
 }
@@ -141,6 +175,9 @@ if (!function_exists('load'))
 {
     function load($view)
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args(), 1, 1);
+        core\traits\ClassMethods::validateArgType(__FUNCTION__, $view, 1, ['string']);
+
         core\base\View::load($view);
     }
 }
@@ -149,6 +186,8 @@ if (!function_exists('strings'))
 {
     function strings()
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args());
+
         return core\utils\Lang::strings();
     }
 }
@@ -157,6 +196,8 @@ if (!function_exists('auth'))
 {
     function auth()
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args());
+
         return (core\utils\Auth::user()) ? new core\utils\Auth() : null;
     }
 }
@@ -168,6 +209,8 @@ if (!function_exists('token'))
      */
     function token()
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args());
+
         return core\utils\Session::token();
     }
 }
@@ -176,6 +219,8 @@ if (!function_exists('session_token'))
 {
     function session_token()
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args());
+
         return core\utils\Session::token();
     }
 }
@@ -187,6 +232,9 @@ if (!function_exists('old'))
      */
     function old($name)
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args(), 1, 1);
+        core\traits\ClassMethods::validateArgType(__FUNCTION__, $name, 1, ['string']);
+
         return core\utils\Request::get($name);
     }
 }
@@ -195,6 +243,9 @@ if (!function_exists('previous'))
 {
     function previous($name)
     {
+        core\traits\ClassMethods::validateNumArgs(__FUNCTION__, func_num_args(), 1, 1);
+        core\traits\ClassMethods::validateArgType(__FUNCTION__, $name, 1, ['string']);
+
         return core\utils\Request::get($name);
     }
 }
